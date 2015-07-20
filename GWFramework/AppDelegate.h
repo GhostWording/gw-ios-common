275d783f76be3105c1cccef6370ef3c0743e31c5
roodoodey <mathieugrettir@hotmail.com>
2015-07-20 22:44:39 +0000

    Initial Commit

---
+++ GWFramework/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GWFramework
+//
+//  Created by Mathieu Skulason on 20/07/15.
+//  Copyright (c) 2015 Mathieu Skulason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GWFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GWFramework
+//
+//  Created by Mathieu Skulason on 20/07/15.
+//  Copyright (c) 2015 Mathieu Skulason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
